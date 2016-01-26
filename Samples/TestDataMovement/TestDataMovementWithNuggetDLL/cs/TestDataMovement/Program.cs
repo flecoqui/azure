f1839a2401d0c1e30bfc38bbd90853b236b6cd20
Frédéric Le Coquil <flecoqui@microsoft.com>
2016-01-26 15:40:42 +0100

    Full command line support

--- Samples/TestDataMovement/TestDataMovementWithNuggetDLL/cs/TestDataMovement/Program.cs
+++ Samples/TestDataMovement/TestDataMovementWithNuggetDLL/cs/TestDataMovement/Program.cs
@@ -16,20 +16,37 @@ using System.Threading.Tasks;
 
 namespace TestDataMovement
 {
+    enum Action
+    {
+        None = 0,
+        Upload,
+        Download,
+        Copy
+    }
 
+    struct ArgsParsed
+    {
+        public Action action;
+        public string ErrorMessage;
+
+        public string FileName;
+        public string ContentType;
+
+        public string StorageAccountName;
+        public string StorageAccountKey;
+        public string ContainerName;
+        public string BlobName;
+
+        public string SourceStorageAccountName;
+        public string SourceStorageAccountKey;
+        public string SourceContainerName;
+        public string SourceBlobName;
+    }
 
     class Program
     {
 
-        private const string SourceFileName = "C:\\Hyper-V\\WS\\ws2012.zip";
-        private const string ContentType = "application/zip";
-        // private const string SourceFileName = "C:\\Hyper-V\\WS\\datafile.txt";
-        //private const string ContentType = "application/txt";
-        private const string ContainerName = "testcsharp";
-          // private const string BlobName = "newnewtest1.txt";
-        private const string BlobName = "newtest.zip";
-        private const string StorageAccountName = "dashtestarm0";
-        private const string StorageAccountKey = "lpPI8ciFdZ6mvu24bP34dE8i9+RjXWDkNn6LfSVo1AwtiUGpefzVldpIqjzq8z/BxmH0O7u/8qD0U1pOyXwwTg==";
+
         public static DateTime StartTime;
 
 
@@ -47,7 +64,7 @@ namespace TestDataMovement
                     new Microsoft.WindowsAzure.Storage.Auth.StorageCredentials(storageAccountName, storageAccountKey), true);
 
                 Microsoft.WindowsAzure.Storage.Blob.CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
-                Microsoft.WindowsAzure.Storage.Blob.CloudBlobContainer container = blobClient.GetContainerReference(ContainerName);
+                Microsoft.WindowsAzure.Storage.Blob.CloudBlobContainer container = blobClient.GetContainerReference(containerName);
 
                 // Create the container if it doesn't already exist.
                 container.CreateIfNotExists();
@@ -79,9 +96,176 @@ namespace TestDataMovement
             }
             return bResult;
         }
+        static void DumpSyntax(string ErrorMessage)
+        {
+            Console.WriteLine("TestDataMovement Application version 1.0");
+            if(!string.IsNullOrEmpty(ErrorMessage)) Console.WriteLine("Error: " + ErrorMessage);
+            Console.WriteLine("Syntax:");
+            Console.WriteLine("TestDataMovement -action <Action> [-filename <FileName>] [-contenttype <ContentType>] ");
+            Console.WriteLine("                 [-storageaccountname <StorageAccountName> -storageaccountkey <StorageAccountKey> ");
+            Console.WriteLine("                 -containername <ContainerName> -blobname <BlobName>] ");
+            Console.WriteLine("                 [-sourcestorageaccountname <StorageAccountName> -sourcestorageaccountkey <StorageAccountKey> ");
+            Console.WriteLine("                 -sourcecontainername <ContainerName> -sourceblobname <BlobName>] ");
+            Console.WriteLine("Where <Action>: 'upload' or 'download' or 'copy'");
+            Console.WriteLine("For instance: ");
+            Console.WriteLine("TestDataMovement -action upload -filename <FileName> -contenttype <ContentType> ");
+            Console.WriteLine("                 -storageaccountname <StorageAccountName> -storageaccountkey <StorageAccountKey> -containername ");
+            Console.WriteLine("                 <ContainerName> -blobname <BlobName>");
+            Console.WriteLine("TestDataMovement -action download -filename <FileName> -contenttype <ContentType> ");
+            Console.WriteLine("                 -sourcestorageaccountname <StorageAccountName> -sourcestorageaccountkey <StorageAccountKey> ");
+            Console.WriteLine("                 -sourcecontainername <ContainerName> -sourceblobname <BlobName>");
+            Console.WriteLine("TestDataMovement -action copy -storageaccountname <StorageAccountName> -storageaccountkey <StorageAccountKey>");
+            Console.WriteLine("                 -containername <ContainerName> -blobname <BlobName> ");
+            Console.WriteLine("                 -sourcestorageaccountname <StorageAccountName> -sourcestorageaccountkey <StorageAccountKey> ");
+            Console.WriteLine("                 -sourcecontainername <ContainerName> -sourceblobname <BlobName>");
+        }
+        static void CheckArgs(ArgsParsed arg)
+        {
+            arg.ErrorMessage = string.Empty;
+
+            if (arg.action == Action.Upload)
+            {
+                if(string.IsNullOrEmpty(arg.FileName) &&
+                    string.IsNullOrEmpty(arg.StorageAccountName) &&
+                    string.IsNullOrEmpty(arg.StorageAccountKey) &&
+                    string.IsNullOrEmpty(arg.BlobName) &&
+                    string.IsNullOrEmpty(arg.ContainerName))
+                    arg.ErrorMessage = string.Empty;
+                else
+                    arg.ErrorMessage = "Missing argument for uplaod scenario";
+
+            }
+            else if (arg.action == Action.Download)
+            {
+                if (string.IsNullOrEmpty(arg.FileName) &&
+                    string.IsNullOrEmpty(arg.SourceStorageAccountName) &&
+                    string.IsNullOrEmpty(arg.SourceStorageAccountKey) &&
+                    string.IsNullOrEmpty(arg.SourceBlobName) &&
+                    string.IsNullOrEmpty(arg.SourceContainerName))
+                    arg.ErrorMessage = string.Empty;
+                else
+                    arg.ErrorMessage = "Missing argument for downlaod scenario";
+            }
+            else if (arg.action == Action.Copy)
+            {
+                if (string.IsNullOrEmpty(arg.SourceStorageAccountName) &&
+                    string.IsNullOrEmpty(arg.SourceStorageAccountKey) &&
+                    string.IsNullOrEmpty(arg.SourceBlobName) &&
+                    string.IsNullOrEmpty(arg.SourceContainerName)&&
+                    string.IsNullOrEmpty(arg.StorageAccountName) &&
+                    string.IsNullOrEmpty(arg.StorageAccountKey) &&
+                    string.IsNullOrEmpty(arg.BlobName) &&
+                    string.IsNullOrEmpty(arg.ContainerName))
+                    arg.ErrorMessage = string.Empty;
+                else
+                    arg.ErrorMessage = "Missing argument for copy scenario";
+
+            }
+            else
+                arg.ErrorMessage = "Unexpected action";
+
+        }
+        static ArgsParsed ParseArgs(string[] args)
+        {
+            ArgsParsed result = new ArgsParsed();
+            result.ErrorMessage = string.Empty;
+            for(int i = 0; i <args.Length; i++ )
+            {
+                switch (args[i])
+                {
+                    case "-action":
+                        if (++i < args.Length)
+                        {
+                            if (args[i] == "upload")
+                                result.action = Action.Upload;
+                            else if (args[i] == "download")
+                                result.action = Action.Download;
+                            else if (args[i] == "copy")
+                                result.action = Action.Copy;
+                            else
+                                result.action = Action.None;
+                        }
+                        break;
+                    case "-filename":
+                        if (++i < args.Length)
+                        {
+                            result.FileName = args[i];
+                        }
+                        break;
+                    case "-contenttype":
+                        if (++i < args.Length)
+                        {
+                            result.ContentType = args[i];
+                        }
+                        break;
+                    case "-storageaccountname":
+                        if (++i < args.Length)
+                        {
+                            result.StorageAccountName = args[i];
+                        }
+                        break;
+                    case "-storageaccountkey":
+                        if (++i < args.Length)
+                        {
+                            result.StorageAccountKey = args[i];
+                        }
+                        break;
+                    case "-containername":
+                        if (++i < args.Length)
+                        {
+                            result.ContainerName = args[i];
+                        }
+                        break;
+                    case "-blobname":
+                        if (++i < args.Length)
+                        {
+                            result.BlobName = args[i];
+                        }
+                        break;
+                    case "-sourcestorageaccountname":
+                        if (++i < args.Length)
+                        {
+                            result.SourceStorageAccountName = args[i];
+                        }
+                        break;
+                    case "-sourcestorageaccountkey":
+                        if (++i < args.Length)
+                        {
+                            result.SourceStorageAccountKey = args[i];
+                        }
+                        break;
+                    case "-sourcecontainername":
+                        if (++i < args.Length)
+                        {
+                            result.SourceContainerName = args[i];
+                        }
+                        break;
+                    case "-sourceblobname":
+                        if (++i < args.Length)
+                        {
+                            result.SourceBlobName = args[i];
+                        }
+                        break;
+                }
+
+            }
+            CheckArgs(result);
+            return result;
+        }
         static void Main(string[] args)
         {
-            UploadFileWithNuggetDLL(SourceFileName, ContentType, StorageAccountName, StorageAccountKey, ContainerName, BlobName);
+            ArgsParsed arg = ParseArgs(args);
+            if (string.IsNullOrEmpty(arg.ErrorMessage))
+            {
+                if(arg.action == Action.Upload)
+                    UploadFileWithNuggetDLL(arg.FileName, arg.ContentType, arg.StorageAccountName, arg.StorageAccountKey, arg.ContainerName, arg.BlobName);
+                else
+                    DumpSyntax("Not implemented");
+            }
+            else
+            {
+                DumpSyntax(arg.ErrorMessage);
+            }
         }
 
 
