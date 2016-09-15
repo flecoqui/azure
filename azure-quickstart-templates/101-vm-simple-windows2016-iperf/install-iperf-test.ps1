eb01566751e94a94e76846321c29ff453433f4c9
Frédéric Le Coquil <flecoqui@microsoft.com>
2016-09-15 11:42:40 +0200

    fix in the ps1 script

--- azure-quickstart-templates/101-vm-simple-windows2016-iperf/install-iperf-test.ps1
+++ azure-quickstart-templates/101-vm-simple-windows2016-iperf/install-iperf-test.ps1
@@ -13,4 +13,34 @@ if(!$dnsName) {
 
 #iperf Installation
 
+$source = 'C:\source' 
+ 
+If (!(Test-Path -Path $source -PathType Container)) {New-Item -Path $source -ItemType Directory | Out-Null} 
+
+Write-Host "Downloading iperf3" 
+$url = 'https://iperf.fr/download/windows/iperf-3.1.3-win64.zip'
+$webClient = New-Object System.Net.WebClient 
+$webClient.DownloadFile($url,$source + "\iperf3.zip" ) 
+
+Write-Host "Installing iperf3" 
+Add-Type -assembly “system.io.compression.filesystem”
+
+[io.compression.zipfile]::ExtractToDirectory($source + "\iperf3.zip",$source)
+
+
+Write-Host "Configuring firewall" 
+Import-Module NetSecurity
+New-NetFirewallRule -Name http -DisplayName “Allow http” -Description “Allow http” -Protocol TCP -LocalPort 80 -Enabled True -Profile Any -Action Allow 
+New-NetFirewallRule -Name iperf3tcp -DisplayName “Allow iperf3 tcp” -Description “Allow iperf3 tcp” -Protocol TCP -LocalPort 5201 -Enabled True -Profile Any -Action Allow 
+New-NetFirewallRule -Name iperf3udp -DisplayName “Allow iperf3 udp” -Description “Allow iperf3 udp” -Protocol UDP -LocalPort 5201 -Enabled True -Profile Any -Action Allow 
+
+function bg() {Start-Process -NoNewWindow @args}
+
+Write-Host "Launching iperf3" 
+$exe = $source + "\iperf-3.1.3-win64\iperf3.exe" 
+bg $exe "-s" 
+
+
+ 
+
 
