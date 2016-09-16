6708a748ffce8439ca242122d44bb3d7ff9e6b8c
Frédéric Le Coquil <flecoqui@microsoft.com>
2016-09-16 10:32:33 +0200

    Change unzip procedure

--- azure-quickstart-templates/101-vm-simple-windows2016-iperf/install-iperf-test.ps1
+++ azure-quickstart-templates/101-vm-simple-windows2016-iperf/install-iperf-test.ps1
@@ -42,18 +42,49 @@ Expand-ZIPFile -file "$source\iperf3.zip" -destination $source
 
 
 Write-Host "Configuring firewall" 
-Import-Module NetSecurity
-New-NetFirewallRule -Name http -DisplayName “Allow http” -Description “Allow http” -Protocol TCP -LocalPort 80 -Enabled True -Profile Any -Action Allow 
-New-NetFirewallRule -Name iperf3tcp -DisplayName “Allow iperf3 tcp” -Description “Allow iperf3 tcp” -Protocol TCP -LocalPort 5201 -Enabled True -Profile Any -Action Allow 
-New-NetFirewallRule -Name iperf3udp -DisplayName “Allow iperf3 udp” -Description “Allow iperf3 udp” -Protocol UDP -LocalPort 5201 -Enabled True -Profile Any -Action Allow 
+function Add-FirewallRules
+{
+netsh advfirewall firewall add rule name="iperfudp" dir=in action=allow protocol=UDP localport=5201
+netsh advfirewall firewall add rule name="iperftcp" dir=in action=allow protocol=TCP localport=5201
+netsh advfirewall firewall add rule name="http" dir=in action=allow protocol=TCP localport=80
+}
+Add-FirewallRules
+#Import-Module NetSecurity
+#New-NetFirewallRule -Name http -DisplayName “Allow http” -Description “Allow http” -Protocol TCP -LocalPort 80 -Enabled True -Profile Any -Action Allow 
+#New-NetFirewallRule -Name iperf3tcp -DisplayName “Allow iperf3 tcp” -Description “Allow iperf3 tcp” -Protocol TCP -LocalPort 5201 -Enabled True -Profile Any -Action Allow 
+#New-NetFirewallRule -Name iperf3udp -DisplayName “Allow iperf3 udp” -Description “Allow iperf3 udp” -Protocol UDP -LocalPort 5201 -Enabled True -Profile Any -Action Allow 
 
-function bg() {Start-Process -NoNewWindow @args}
+function bg() {Start-Job -scriptblock { @args }}
 
 Write-Host "Launching iperf3" 
 $exe = $source + "\iperf-3.1.3-win64\iperf3.exe" 
 bg $exe "-s" 
 
+Write-Host "Creating Home Page" 
+$dnsName="toot.ttott.com"
+$content = @'
+<html>
+  <head>
+    <title>Sample "Hello from {0}" </title>
+  </head>
+  <body bgcolor=white>
+    <table border="0" cellpadding="10">
+      <tr>
+        <td>
+          <h1>Hello from {0}</h1>
+        </td>
+      </tr>
+    </table>
 
- 
-
-
+    <p>This is the home page for the iperf3 test on Azure VM</p>
+    <p>Launch the command line from your client: </p>
+    <p>     iperf3 -c {0} -p 5201 --parallel 32  </p>
+    <ul>
+      <li>To <a href="http://www.microsoft.com">Microsoft</a>
+      <li>To <a href="https://portal.azure.com">Azure</a>
+    </ul>
+  </body>
+</html>
+'@
+$content = $content -replace "\{0\}",$dnsName
+$content | Out-File -FilePath C:\inetpub\wwwroot\index.html -Encoding utf8
