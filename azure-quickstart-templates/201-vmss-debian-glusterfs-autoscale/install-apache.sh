5cfc259d9e2e542943a277558539919e786ec765
Frédéric Le Coquil <flecoqui@microsoft.com>
2016-10-24 13:56:09 +0200

    Add 201-vmmss-debian-glusterfs

---
+++ azure-quickstart-templates/201-vmss-debian-glusterfs-autoscale/install-apache.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# This bash file install apache on debian
+# Parameter 1 hostname 
+wm_hostname=$1
+wm_ipaddr=`ip addr show  eth0 | grep global`
+
+environ=`env`
+echo "Environment before installation: $environ"
+
+echo "Installation script start : $(date)"
+echo "Apache Installation: $(date)"
+echo "#####  wm_hostname: $wm_hostname"
+echo "Installation script start : $(date)"
+# Apache installation 
+apt-get -y update
+apt-get -y install apache2
+# firewall configuration 
+iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+
+directory=/var/www/html
+if [ ! -d $directory ]; then
+mkdir $directory
+fi
+
+cat <<EOF > $directory/index.html 
+<html>
+  <head>
+    <title>Sample "Hello from Azure Debian VM scaleset $wm_hostname" </title>
+  </head>
+  <body bgcolor=white>
+
+    <table border="0" cellpadding="10">
+      <tr>
+        <td>
+          <h1>Hello from Azure Debian VM scaleset $wm_hostname</h1>
+        </td>
+      </tr>
+    </table>
+
+    <p>This is the home page for the Apache test on Azure VM</p>
+    <p>Local IP address:</p>
+    <p>   $wm_ipaddr</p> 
+    <ul>
+      <li>To the VM Scale Set: <a href="http://$wm_hostname/html/index.html">http://$wm_hostname/html/index.html</a>
+    </ul>
+    <ul>
+      <li>To <a href="http://www.microsoft.com">Microsoft</a>
+      <li>To <a href="https://portal.azure.com">Azure</a>
+    </ul>
+  </body>
+</html>
+EOF
+
+
+apachectl restart
+exit 0 
+
