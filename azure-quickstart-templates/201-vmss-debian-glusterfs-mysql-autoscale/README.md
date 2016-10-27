face2bcc55d36d2ae0eeb0d9264eeeb6b23cdda0
Frédéric Le Coquil <flecoqui@microsoft.com>
2016-10-27 10:30:11 +0200

    Add debian glusterfs and mysql template

---
+++ azure-quickstart-templates/201-vmss-debian-glusterfs-mysql-autoscale/README.md
@@ -0,0 +1,33 @@
+# Deployment of a VM scale set with Apache server, GlusterFS NFS partition and mySQL DB 
+
+<a href="https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2Fflecoqui%2Fazure%2Fazure-quickstart-templates%2Fmaster%2F101-vm-simple-centos-iperf%2Fazuredeploy.json" target="_blank">
+    <img src="http://azuredeploy.net/deploybutton.png"/>
+</a>
+<a href="http://armviz.io/#/?load=https%3A%2F%2Fraw.githubusercontent.com%2Fflecoqui%2Fazure%2F%2Fazure-quickstart-templates%2Fmaster%2F101-vm-simple-centos-iperf%2Fazuredeploy.json" target="_blank">
+    <img src="http://armviz.io/visualizebutton.png"/>
+</a>
+
+
+This template allows you to deploy a scaleset of Debian VM running Apache with GlusterFS NFS partition and mySQL DB server (replica), using the latest patched version. This will deploy in the region associated with Resource Group and the VM Size is one of the parameter.
+With Azure CLI you can deploy this VM with 2 command lines:
+
+##CREATE RESOURCE GROUP:
+azure group create "ResourceGroupName" "DataCenterName"
+
+For instance:
+
+    azure group create vmssdebianrg eastus2
+
+##DEPLOY THE VM:
+azure group deployment create "ResourceGroupName" "DeploymentName"  -f azuredeploy.json -e azuredeploy.parameters.json
+
+For instance:
+
+    azure group deployment create vmssdebianrg devmssdebian -f azuredeploy.json -e azuredeploy.parameters.json -vv
+
+##DELETE THE RESOURCE GROUP:
+azure group delete "ResourceGroupName" "DataCenterName"
+
+For instance:
+
+    azure group delete vmssdebianrg eastus2
---
+++ azure-quickstart-templates/201-vmss-debian-glusterfs-mysql-autoscale/install-apache.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+# This bash file install apache on debian
+# Parameter 1 hostname 
+wm_hostname=$1
+wm_ipaddr=`ip addr show  eth0 | grep global`
+
+environ=`env`
+echo "Environment before installation: $environ"
+
+echo "Installation script start : $(date)"
+echo "Apache Installation: $(date)"
+echo "#####  wm_hostname: $wm_hostname"
+echo "Installation script start : $(date)"
+# Apache installation 
+apt-get -y update
+apt-get -y install apache2
+# GlusterFS client  installation 
+# install a version compliant with Debian 7 
+wget -O - http://download.gluster.org/pub/gluster/glusterfs/3.7/LATEST/rsa.pub | apt-key add -
+echo deb http://download.gluster.org/pub/gluster/glusterfs/3.7/LATEST/Debian/wheezy/apt wheezy main > /etc/apt/sources.list.d/gluster.list 
+#wget -O - http://download.gluster.org/pub/gluster/glusterfs/LATEST/rsa.pub | apt-key add -
+#echo deb http://download.gluster.org/pub/gluster/glusterfs/LATEST/Debian/jessie/apt jessie main > /etc/apt/sources.list.d/gluster.list
+apt-get update -y
+apt-get install glusterfs-client -y
+
+# firewall configuration 
+iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+
+# glusterfs mount
+mkdir /shareddata
+mount -t glusterfs gfs1vm0:gfs1vol /shareddata
+wm_nfs=`df -h /shareddata`
+
+directory=/var/www/html
+if [ ! -d $directory ]; then
+mkdir $directory
+fi
+
+cat <<EOF > $directory/index.html 
+<html>
+  <head>
+    <title>Sample "Hello from Azure Debian VM scaleset $wm_hostname" </title>
+  </head>
+  <body bgcolor=white>
+
+    <table border="0" cellpadding="10">
+      <tr>
+        <td>
+          <h1>Hello from Azure Debian VM scaleset $wm_hostname</h1>
+        </td>
+      </tr>
+    </table>
+
+    <p>This is the home page for the Apache test on Azure VM</p>
+    <p>Local IP address:</p>
+    <p>   $wm_ipaddr</p> 
+    <p>NFS partition:</p>
+    <p>   $wm_nfs</p> 
+    <ul>
+      <li>To the VM Scale Set: <a href="http://$wm_hostname/html/index.html">http://$wm_hostname/html/index.html</a>
+    </ul>
+    <ul>
+      <li>To <a href="http://www.microsoft.com">Microsoft</a>
+      <li>To <a href="https://portal.azure.com">Azure</a>
+    </ul>
+  </body>
+</html>
+EOF
+
+apachectl restart
+exit 0 
+
