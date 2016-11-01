2d960b5831df03bc7be847284f6dd30d06b61038
Frédéric Le Coquil <flecoqui@microsoft.com>
2016-11-01 22:49:54 +0100

    Update the script

--- azure-quickstart-templates/201-vmss-debian-redis-autoscale/install-apache.sh
+++ azure-quickstart-templates/201-vmss-debian-redis-autoscale/install-apache.sh
@@ -12,6 +12,52 @@ echo "#####  wm_hostname: $wm_hostname"
 echo "Installation script start : $(date)"
 ARCH=$(uname -m | sed 's/x86_//;s/i[3-6]86/32/')
 
+#############################################################################
+VERSION="3.0.0"
+REDIS_PORT=6379
+#############################################################################
+install_redis()
+{
+	log "Installing Redis v${VERSION}"
+
+	# Installing build essentials (if missing) and other required tools
+	apt-get -y install build-essential
+
+	wget http://download.redis.io/releases/redis-$VERSION.tar.gz
+	tar xzf redis-$VERSION.tar.gz
+	cd redis-$VERSION
+	make
+	make install prefix=/usr/local/bin/
+
+	log "Redis package v${VERSION} was downloaded and built successfully"
+}
+
+#############################################################################
+configure_redis()
+{
+	# Configure the general settings
+	sed -i "s/^port.*$/port ${REDIS_PORT}/g" redis.conf
+	sed -i "s/^daemonize no$/daemonize yes/g" redis.conf
+	sed -i 's/^logfile ""/logfile \/var\/log\/redis.log/g' redis.conf
+	sed -i "s/^loglevel verbose$/loglevel notice/g" redis.conf
+	sed -i "s/^dir \.\//dir \/var\/redis\//g" redis.conf 
+	sed -i "s/\${REDISPORT}.conf/redis.conf/g" utils/redis_init_script 
+	sed -i "s/_\${REDISPORT}.pid/.pid/g" utils/redis_init_script 
+	
+	# Create all essentials directories and copy files to the correct locations
+	mkdir /etc/redis
+	mkdir /var/redis
+	
+	cp redis.conf /etc/redis/redis.conf
+	
+	# Clean up temporary files
+	cd ..
+	rm redis-$VERSION -R
+	rm redis-$VERSION.tar.gz
+
+	log "Redis configuration was applied successfully"
+}
+
 if [ -f /etc/lsb-release ]; then
     . /etc/lsb-release
     OS=$DISTRIB_ID
@@ -31,32 +77,19 @@ echo "OS=$OS version $VER $ARCH"
 # Apache installation 
 apt-get -y update
 apt-get -y install apache2
-# GlusterFS client  installation 
-NUM=`echo $VER |  sed 's/\(\.\)[0-9]*//g'`
-if [ $NUM -eq 7 ];
-then
-# install a version compliant with Debian 7 
-wget -O - http://download.gluster.org/pub/gluster/glusterfs/3.7/LATEST/rsa.pub | apt-key add -
-echo deb http://download.gluster.org/pub/gluster/glusterfs/3.7/LATEST/Debian/wheezy/apt wheezy main > /etc/apt/sources.list.d/gluster.list 
-fi
-if [ $NUM -eq 8 ];
-then
-wget -O - http://download.gluster.org/pub/gluster/glusterfs/LATEST/rsa.pub | apt-key add -
-echo deb http://download.gluster.org/pub/gluster/glusterfs/LATEST/Debian/jessie/apt jessie main > /etc/apt/sources.list.d/gluster.list
-fi
 
-apt-get update -y
-apt-get install glusterfs-client -y
+# Redis installation & configuration 
+install_redis()
+configure_redis()
+
 
 # firewall configuration 
 iptables -A INPUT -p tcp --dport 80 -j ACCEPT
 iptables -A INPUT -p tcp --dport 443 -j ACCEPT
 iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 
-# glusterfs mount
-mkdir /shareddata
-mount -t glusterfs gfs1vm0:gfs1vol /shareddata
-wm_nfs=`df -h /shareddata`
+# redis test 
+wm_redis=`redis-cli -h 10.0.0.80 ping`
 
 directory=/var/www/html
 if [ ! -d $directory ]; then
@@ -81,8 +114,8 @@ cat <<EOF > $directory/index.html
     <p>This is the home page for the Apache test on Azure VM</p>
     <p>Local IP address:</p>
     <p>   $wm_ipaddr</p> 
-    <p>NFS partition:</p>
-    <p>   $wm_nfs</p> 
+    <p>Redis Cache response to ping:</p>
+    <p>   $wm_redis</p> 
     <ul>
       <li>To the VM Scale Set: <a href="http://$wm_hostname/html/index.html">http://$wm_hostname/html/index.html</a>
     </ul>
