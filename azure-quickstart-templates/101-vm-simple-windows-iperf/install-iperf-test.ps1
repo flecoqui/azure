2527a133c9cf3219afa09d098e1cf38b7c3f803e
Frédéric Le Coquil <flecoqui@microsoft.com>
2016-12-04 19:31:39 +0100

    create Windows template

--- azure-quickstart-templates/101-vm-simple-windows-iperf/install-iperf-test.ps1
+++ azure-quickstart-templates/101-vm-simple-windows-iperf/install-iperf-test.ps1
@@ -22,73 +22,45 @@ if(!$dnsName) {
  throw "DNSName not specified"
 }
 
+ WriteLog "Downloading iperf3"  
+ $url = 'https://iperf.fr/download/windows/iperf-3.1.3-win64.zip' 
+ $webClient = New-Object System.Net.WebClient  
+ $webClient.DownloadFile($url,$source + "\iperf3.zip" )  
+ 
+ 
+ WriteLog "Installing iperf3"  
+ # Function to unzip file contents 
+ function Expand-ZIPFile($file, $destination) 
+ { 
+     $shell = new-object -com shell.application 
+     $zip = $shell.NameSpace($file) 
+     foreach($item in $zip.items()) 
+     { 
+         # Unzip the file with 0x14 (overwrite silently) 
+         $shell.Namespace($destination).copyhere($item, 0x14) 
+     } 
+ } 
+Expand-ZIPFile -file "$source\iperf3.zip" -destination $source 
+WriteLog "iperf3 Installed"  
 
-WriteLog "Downloading iperf3" 
-$url = 'https://iperf.fr/download/windows/iperf-3.1.3-win64.zip'
-
-WriteLog "Installing iperf3" 
-
-function DownloadAndUnzip($sourceUrl,$DestinationDir ) 
-{
-$EditionId = (Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion' -Name 'EditionID').EditionId
-
-if (($EditionId -eq "ServerStandardNano") -or
-    ($EditionId -eq "ServerDataCenterNano") -or
-    ($EditionId -eq "NanoServer") -or
-    ($EditionId -eq "ServerTuva")) {
-
-    $TempPath = [System.IO.Path]::GetTempFileName()
-    if (($sourceUrl -as [System.URI]).AbsoluteURI -ne $null)
-    {
-        $handler = New-Object System.Net.Http.HttpClientHandler
-        $client = New-Object System.Net.Http.HttpClient($handler)
-        $client.Timeout = New-Object System.TimeSpan(0, 30, 0)
-        $cancelTokenSource = [System.Threading.CancellationTokenSource]::new()
-        $responseMsg = $client.GetAsync([System.Uri]::new($sourceUrl), $cancelTokenSource.Token)
-        $responseMsg.Wait()
-        if (!$responseMsg.IsCanceled)
-        {
-            $response = $responseMsg.Result
-            if ($response.IsSuccessStatusCode)
-            {
-                $downloadedFileStream = [System.IO.FileStream]::new($TempPath, [System.IO.FileMode]::Create, [System.IO.FileAccess]::Write)
-                $copyStreamOp = $response.Content.CopyToAsync($downloadedFileStream)
-                $copyStreamOp.Wait()
-                $downloadedFileStream.Close()
-                if ($copyStreamOp.Exception -ne $null)
-                {
-                    throw $copyStreamOp.Exception
-                }
-            }
-        }
-    }
-    else
-    {
-        throw "Cannot copy from $sourceUrl"
-    }
-    [System.IO.Compression.ZipFile]::ExtractToDirectory($TempPath, $DestinationDir)
-    Remove-Item $TempPath
-   }
-}
-DownloadAndUnzip $url $source 
-WriteLog "iperf3 Installed" 
 
 
 WriteLog "Configuring firewall" 
 function Add-FirewallRules
 {
-New-NetFirewallRule -Name "IPERFUDP" -DisplayName "IPERF on UDP/5201" -Protocol UDP -LocalPort 5201 -Action Allow -Enabled True
-New-NetFirewallRule -Name "IPERFTCP" -DisplayName "IPERF on TCP/5201" -Protocol TCP -LocalPort 5201 -Action Allow -Enabled True
-New-NetFirewallRule -Name "HTTP" -DisplayName "HTTP" -Protocol TCP -LocalPort 80 -Action Allow -Enabled True
-New-NetFirewallRule -Name "HTTPS" -DisplayName "HTTPS" -Protocol TCP -LocalPort 443 -Action Allow -Enabled True
-New-NetFirewallRule -Name "WINRM1" -DisplayName "WINRM TCP/5985" -Protocol TCP -LocalPort 5985 -Action Allow -Enabled True
-New-NetFirewallRule -Name "WINRM2" -DisplayName "WINRM TCP/5986" -Protocol TCP -LocalPort 5986 -Action Allow -Enabled True
+#New-NetFirewallRule -Name "IPERFUDP" -DisplayName "IPERF on UDP/5201" -Protocol UDP -LocalPort 5201 -Action Allow -Enabled True
+#New-NetFirewallRule -Name "IPERFTCP" -DisplayName "IPERF on TCP/5201" -Protocol TCP -LocalPort 5201 -Action Allow -Enabled True
+#New-NetFirewallRule -Name "HTTP" -DisplayName "HTTP" -Protocol TCP -LocalPort 80 -Action Allow -Enabled True
+#New-NetFirewallRule -Name "HTTPS" -DisplayName "HTTPS" -Protocol TCP -LocalPort 443 -Action Allow -Enabled True
+#New-NetFirewallRule -Name "WINRM1" -DisplayName "WINRM TCP/5985" -Protocol TCP -LocalPort 5985 -Action Allow -Enabled True
+#New-NetFirewallRule -Name "WINRM2" -DisplayName "WINRM TCP/5986" -Protocol TCP -LocalPort 5986 -Action Allow -Enabled True
 
-#netsh advfirewall firewall add rule name="iperfudp" dir=in action=allow protocol=UDP localport=5201
-#netsh advfirewall firewall add rule name="iperftcp" dir=in action=allow protocol=TCP localport=5201
-#netsh advfirewall firewall add rule name="http" dir=in action=allow protocol=TCP localport=80
-#netsh advfirewall firewall add rule name="winrm1" dir=in action=allow protocol=TCP localport=5985
-#netsh advfirewall firewall add rule name="winrm2" dir=in action=allow protocol=TCP localport=5986
+netsh advfirewall firewall add rule name="iperfudp" dir=in action=allow protocol=UDP localport=5201
+netsh advfirewall firewall add rule name="iperftcp" dir=in action=allow protocol=TCP localport=5201
+netsh advfirewall firewall add rule name="http" dir=in action=allow protocol=TCP localport=80
+netsh advfirewall firewall add rule name="http" dir=in action=allow protocol=TCP localport=3389
+netsh advfirewall firewall add rule name="winrm1" dir=in action=allow protocol=TCP localport=5985
+netsh advfirewall firewall add rule name="winrm2" dir=in action=allow protocol=TCP localport=5986
 }
 Add-FirewallRules
 WriteLog "Firewall configured" 
@@ -175,6 +147,9 @@ function Install-script
 $action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-NoExit c:\source\installiis.ps1" 
 $trigger = New-ScheduledTaskTrigger -AtStartup
 #Register-ScheduledTask -TaskName "scriptiis" -Action $action -Trigger $trigger -RunLevel Highest -User $adminUser | Out-Null 
+#function bg() {Invoke-Command -scriptblock  { c:\source\iperf-3.1.3-win64\iperf3.exe -s -D --logfile iperflog.txt }}
+function bg() {Start-Job { c:\source\iperf-3.1.3-win64\iperf3.exe -s -D --logfile c:\source\iperflog.txt }}
+bg
 
 WriteLog "Initialization completed !" 
 WriteLog "Rebooting !" 
