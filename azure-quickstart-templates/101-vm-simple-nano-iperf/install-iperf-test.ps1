9fd51757f927b850a77fa4027cd06b8eb6bbb919
Frédéric Le Coquil <flecoqui@microsoft.com>
2016-12-05 18:13:08 +0100

    Add template ARM for nano server

--- azure-quickstart-templates/101-vm-simple-nano-iperf/install-iperf-test.ps1
+++ azure-quickstart-templates/101-vm-simple-nano-iperf/install-iperf-test.ps1
@@ -136,11 +136,10 @@ function Install-IIS
 function Install-script
 {
  "
- function bg() {Invoke-Command -scriptblock  { c:\source\iperf-3.1.3-win64\iperf3.exe -s -D --logfile iperflog.txt }}`r`n
+ #function bg() {Invoke-Command -scriptblock  { c:\source\iperf-3.1.3-win64\iperf3.exe -s -D --logfile iperflog.txt }}`r`n
  if (!(Test-Path -Path c:\source\iis.log)) `r`n
  {  `r`n
-
- Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+ Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force `r`n
   echo install-Module-NameNanoServerPackage-SkipPublisherCheck > c:\source\iis.log `r`n 
  install-Module -Name NanoServerPackage -SkipPublisherCheck -force `r`n
  echo install-PackagePRoviderNanoServerPackage >> c:\source\iis.log `r`n 
@@ -156,24 +155,23 @@ function Install-script
  echo Restart-Computer >> c:\source\iis.log `r`n 
  Restart-Computer -Force      `r`n 
  } `r`n
- else `r`n
- { `r`n
- bg `r`n
- } `r`n
- `r`n
+ #else `r`n
+ #{ `r`n
+ #bg `r`n
+ #} `r`n
  "
 }
-Install-IIS
-#Install-script > c:\source\installiis.ps1
+#Install-IIS
+Install-script > c:\source\installiis.ps1
 
 WriteLog "Installing IPERF3 as a service" 
 sc.exe create ipef3 binpath= "cmd.exe /c c:\source\iperf-3.1.3-win64\iperf3.exe -s -D" type= own start= auto DisplayName= "IPERF3"
 WriteLog "IPERF3 Installed"
 
 #create scheduled task
-#$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-NoExit c:\source\installiis.ps1" 
-#$trigger = New-ScheduledTaskTrigger -AtStartup
-#Register-ScheduledTask -TaskName "scriptiis" -Action $action -Trigger $trigger -RunLevel Highest -User $adminUser | Out-Null 
+$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-NoExit c:\source\installiis.ps1" 
+$trigger = New-ScheduledTaskTrigger -AtStartup
+Register-ScheduledTask -TaskName "scriptiis" -Action $action -Trigger $trigger -RunLevel Highest -User $adminUser | Out-Null 
 
 WriteLog "Initialization completed !" 
 WriteLog "Rebooting !" 
