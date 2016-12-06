860ce16828d9a651bc6ecacf1aaee464516cf2f1
Frédéric Le Coquil <flecoqui@microsoft.com>
2016-12-06 13:57:52 +0100

    update ps1 file for nano

--- azure-quickstart-templates/101-vm-simple-nano-iperf/install-iperf-test.ps1
+++ azure-quickstart-templates/101-vm-simple-nano-iperf/install-iperf-test.ps1
@@ -124,10 +124,6 @@ $content | Out-File -FilePath C:\inetpub\wwwroot\index.html -Encoding utf8
  
 function Install-IIS
 {
-#Install-PackageProvider NanoServerPackage
- #  Import-PackageProvider NanoServerPackage
-  # Install-NanoServerPackage -Name Microsoft-NanoServer-Storage-Package
-   #Install-NanoServerPackage -Name Microsoft-NanoServer-IIS-Package
 WriteLog "Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force"
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 WriteLog "Save-Module -Path "$env:programfiles\WindowsPowerShell\Modules\" -Name NanoServerPackage -minimumVersion 1.0.1.0"
@@ -139,131 +135,24 @@ Find-NanoServerPackage -name *
 WriteLog "Find-NanoServerPackage *iis* | install-NanoServerPackage -culture en-us"
 Find-NanoServerPackage *iis* | install-NanoServerPackage -culture en-us
 
-# WriteLog "Installing IIS: Install-Module -Name NanoServerPackage -SkipPublisherCheck -force"
-# Install-Module -Name NanoServerPackage -SkipPublisherCheck -force
- 
-# WriteLog "Installing IIS: Install-PackageProvider NanoServerPackage" 
-# Install-PackageProvider NanoServerPackage
- 
-# WriteLog "Installing IIS:Set-ExecutionPolicy RemoteSigned -Scope Process" 
-# Set-ExecutionPolicy RemoteSigned -Scope Process
-
-# WriteLog "Installing IIS: Import-PackageProvider NanoServerPackage" 
-# Import-PackageProvider NanoServerPackage
-
-# WriteLog "Installing IIS: Find-NanoServerPackage –AllVersions -Name *IIS* -RequiredVersion 10.0.14393.0"
-# Find-NanoServerPackage –AllVersions -Name *IIS* -RequiredVersion 10.0.14393.0
-
-# WriteLog "Installing IIS: Install-NanoServerPackage -Name Microsoft-NanoServer-IIS-Package -Culture en-us -RequiredVersion 10.0.14393.0"
-# Install-NanoServerPackage -Name Microsoft-NanoServer-IIS-Package -Culture en-us -RequiredVersion 10.0.14393.0
-
- WriteLog "Installing IIS: done"
-# install-Module -Name NanoServerPackage -SkipPublisherCheck -force
-# install-PackageProvider NanoServerPackage
-# Set-ExecutionPolicy RemoteSigned -Scope Process
-# Import-PackageProvider NanoServerPackage
-# Install-NanoServerPackage -Name Microsoft-NanoServer-Storage-Package
-# Install-NanoServerPackage -Name Microsoft-NanoServer-IIS-Package
 }
 
-function Dump-Install-IIS
-{
- "
- function WriteLog(`$msg) `r
- { `r
- Write-Host `$msg `r
- `$msg >> c:\source\installiis.log `r
- } `r
- function WriteDateLog `r
- { `r
- date >> c:\source\installiis.log `r
- } `r
- if (!(Test-Path -Path c:\source\iis.log)) `r
- {  `r
- WriteLog ""Installing IIS: Install-Module -Name NanoServerPackage -SkipPublisherCheck -force "" `r
- Install-Module -Name NanoServerPackage -SkipPublisherCheck -force `r
- WriteLog ""Installing IIS: Install-PackageProvider NanoServerPackage"" `r
- Install-PackageProvider NanoServerPackage `r 
- WriteLog ""Installing IIS:Set-ExecutionPolicy RemoteSigned -Scope Process""  `r
- Set-ExecutionPolicy RemoteSigned -Scope Process `r
- WriteLog ""Installing IIS: Import-PackageProvider NanoServerPackage"" `r 
- Import-PackageProvider NanoServerPackage `r
- WriteLog ""Installing IIS: Find-NanoServerPackage –AllVersions -Name *IIS* -RequiredVersion 10.0.14393.0"" `r
- Find-NanoServerPackage –AllVersions -Name *IIS* -RequiredVersion 10.0.14393.0 `r
- WriteLog ""Installing IIS: Install-NanoServerPackage -Name Microsoft-NanoServer-IIS-Package -Culture en-us -RequiredVersion 10.0.14393.0"" `r
- Install-NanoServerPackage -Name Microsoft-NanoServer-IIS-Package -Culture en-us -RequiredVersion 10.0.14393.0 `r
- WriteLog ""Installing IIS: done"" `r
- WriteLog ""Starting IIS""  `r
- net start w3svc `r
- WriteLog ""Starting IIS: done"" `r
- date >> c:\source\iis.log `r
- }`r
- "
-}
-
-function Install-script
-{
- "
- #function bg() {Invoke-Command -scriptblock  { c:\source\iperf-3.1.3-win64\iperf3.exe -s -D --logfile iperflog.txt }}`r`n
- if (!(Test-Path -Path c:\source\iis.log)) `r`n
- {  `r`n
- echo Start installation script >> c:\source\iis.log `r`n 
- echo date >> c:\source\iis.log `r`n 
- echo Set-ExecutionPolicyRemoteSigned-ScopeProcess >> c:\source\iis.log `r`n 
- Set-ExecutionPolicy RemoteSigned -Scope Process `r`n
- Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force `r`n
- echo install-Module-NameNanoServerPackage-SkipPublisherCheck >> c:\source\iis.log `r`n 
- install-Module -Name NanoServerPackage -SkipPublisherCheck -force `r`n
- echo install-PackagePRoviderNanoServerPackage >> c:\source\iis.log `r`n 
- install-PackagePRovider NanoServerPackage `r`n
- echo Import-PackageProviderNanoServerPackage >> c:\source\iis.log `r`n 
- Import-PackageProvider NanoServerPackage `r`n
- echo Install-NanoServerPackage-NameMicrosoft-NanoServer-Storage-Package >> c:\source\iis.log `r`n 
- Install-NanoServerPackage -Name Microsoft-NanoServer-Storage-Package `r`n
- echo Install-NanoServerPackage-Name Microsoft-NanoServer-IIS-Package >> c:\source\iis.log `r`n 
- Install-NanoServerPackage -Name Microsoft-NanoServer-IIS-Package `r`n
- echo Restart-Computer >> c:\source\iis.log `r`n 
- Restart-Computer -Force      `r`n 
- } `r`n
- else `r`n
- { `r`n
- echo Start installation script Nothing to do  >> c:\source\iis.log `r`n 
- echo date >> c:\source\iis.log `r`n 
- #bg `r`n
- } `r`n
- "
-}
 
 WriteDateLog
+WriteLog "Installing IIS"
 Install-IIS
+WriteLog "Installing IIS: done"
 
 WriteLog "Starting IIS" 
 net start w3svc
 
-#Install-script > c:\source\installiis.ps1
-#WriteLog "Create file c:\source\installiis.ps1" 
-#Dump-Install-IIS > c:\source\installiis.ps1
+
 
 WriteDateLog
 WriteLog "Installing IPERF3 as a service" 
 sc.exe create ipef3 binpath= "cmd.exe /c c:\source\iperf-3.1.3-win64\iperf3.exe -s -D" type= own start= auto DisplayName= "IPERF3"
 WriteLog "IPERF3 Installed"
 
-
-#WriteLog "Create file c:\source\installiis.ps1" 
-#Dump-Install-IIS > c:\source\installiis.ps1
-#create scheduled task
-#WriteLog "Create scheduled task for file c:\source\installiis.ps1" 
-#$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-NoExit c:\source\installiis.ps1" 
-#$trigger = New-ScheduledTaskTrigger -AtLogOn
-#Register-ScheduledTask -TaskName "scriptiis" -Action $action -Trigger $trigger -RunLevel Highest -User $adminUser | Out-Null 
-
-
-
-#echo "echo ""boot at"" >> c:\source\Startup.log"    > c:\source\Startup.cmd
-#echo "date >> c:\source\Startup.log"    >> c:\source\Startup.cmd
-#echo "ipconfig >> c:\source\Startup.log"   >> c:\source\Startup.cmd 
-#schtasks /create /tn "Logboot" /tr c:\source\Startup.cmd /sc onstart /ru "System"
 
 WriteLog "Initialization completed !" 
 WriteDateLog
