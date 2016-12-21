57861ba4567eea863351ffc790dd006d225b804d
Frédéric Le Coquil <flecoqui@microsoft.com>
2016-12-21 08:26:55 +0100

    add debian netcore template

---
+++ azure-quickstart-templates/101-vm-simple-debian-netcore/README.md
@@ -0,0 +1,33 @@
+# Very simple deployment of an Debian VM running Apache (port 80) and .Net Core
+
+<a href="https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2Fflecoqui%2Fazure%2Fmaster%2Fazure-quickstart-templates%2F101-vm-simple-debian-netcore%2Fazuredeploy.json" target="_blank">
+    <img src="http://azuredeploy.net/deploybutton.png"/>
+</a>
+<a href="http://armviz.io/#/?load=https%3A%2F%2Fraw.githubusercontent.com%2Fflecoqui%2Fazure%2Fmaster%2Fazure-quickstart-templates%2F101-vm-simple-debian-netcore%2Fazuredeploy.json" target="_blank">
+    <img src="http://armviz.io/visualizebutton.png"/>
+</a>
+
+
+This template allows you to deploy a simple Debian VM running Apache and .Net Core, using the latest patched version. This will deploy in the region associated with Resource Group and the VM Size is one of the parameter.
+With Azure CLI you can deploy this VM with 2 command lines:
+
+##CREATE RESOURCE GROUP:
+azure group create "ResourceGroupName" "DataCenterName"
+
+For instance:
+
+    azure group create netcoregrpeu2 eastus2
+
+##DEPLOY THE VM:
+azure group deployment create "ResourceGroupName" "DeploymentName"  -f azuredeploy.json -e azuredeploy.parameters.json
+
+For instance:
+
+    azure group deployment create netcoregrpeu2 depiperftest -f azuredeploy.json -e azuredeploy.parameters.json -vv
+
+##DELETE THE RESOURCE GROUP:
+azure group delete "ResourceGroupName" "DataCenterName"
+
+For instance:
+
+    azure group delete netcoregrpeu2 eastus2
---
+++ azure-quickstart-templates/101-vm-simple-debian-netcore/install-netcore-test.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+# This bash file install iperf3 demonstraiton on centos
+# Parameter 1 hostname 
+usp_hostname=$1
+
+
+environ=`env`
+echo "Environment before installation: $environ"
+
+echo "Installation script start : $(date)"
+echo "Apache Installation: $(date)"
+echo "#####  usp_hostname: $usp_hostname"
+echo "Installation script start : $(date)"
+# Apache installation 
+apt-get -y update
+apt-get -y install apache2
+# firewall configuration 
+sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+#
+# install .Net Core
+#  
+apt-get install curl libunwind8 gettext
+curl -sSL -o dotnet.tar.gz https://go.microsoft.com/fwlink/?LinkID=835021
+sudo mkdir -p /opt/dotnet && sudo tar zxf dotnet.tar.gz -C /opt/dotnet
+sudo ln -s /opt/dotnet/dotnet /usr/local/bin
+
+# Create new app
+#mkdir hwapp
+#cd hwapp
+#dotnet new
+#dotnet restore
+#dotnet run
+
+#
+# Start Apache server
+#
+systemctl start apache2
+systemctl enable apache2
+systemctl status apache2
+
+directory=/var/www/html
+if [ ! -d $directory ]; then
+mkdir $directory
+fi
+
+cat <<EOF > $directory/index.html 
+<html>
+  <head>
+    <title>Sample "Hello from $usp_hostname" </title>
+  </head>
+  <body bgcolor=white>
+
+    <table border="0" cellpadding="10">
+      <tr>
+        <td>
+          <h1>Hello from $usp_hostname</h1>
+        </td>
+      </tr>
+    </table>
+
+    <p>This is the home page for .Net Core test on Azure VM</p>
+    <ul>
+      <li>To <a href="http://www.microsoft.com">Microsoft</a>
+      <li>To <a href="https://portal.azure.com">Azure</a>
+    </ul>
+  </body>
+</html>
+EOF
+
+
+echo "Configuring Web Site for Apache: $(date)"
+cat <<EOF > /etc/apache2/conf-available/test-web.conf 
+ServerName "$usp_hostname"
+<VirtualHost *:80>
+        ServerAdmin webmaster@localhost
+        ServerName "$usp_hostname"
+
+        DocumentRoot /var/www/test-web
+        <Directory />
+                Options FollowSymLinks
+                AllowOverride None
+        </Directory>
+
+       # Add CORS headers for HTML5 players
+        Header always set Access-Control-Allow-Headers "origin, range"
+        Header always set Access-Control-Allow-Methods "GET, HEAD, OPTIONS"
+        Header always set Access-Control-Allow-Origin "*"
+        Header always set Access-Control-Expose-Headers "Server,range"
+
+        # Possible values include: debug, info, notice, warn, error, crit,
+        # alert, emerg.
+        LogLevel warn
+        ErrorLog /var/log/httpd/usp-evaluation-error.log
+        CustomLog /var/log/httpd/usp-evaluation-access.log combined
+</VirtualHost>
+EOF
+apachectl restart
+exit 0 
+
