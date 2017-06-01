ddff4d4cd1e5e2b5d722a84a2c6362d414486ef7
Frédéric Le Coquil <flecoqui@microsoft.com>
2017-06-01 19:30:01 +0200

    update NAT and port forwarding

--- azure-quickstart-templates/101-vm-bridge-linux/install-bridge.sh
+++ azure-quickstart-templates/101-vm-bridge-linux/install-bridge.sh
@@ -21,21 +21,30 @@ iptables -A INPUT -p udp --dport 5201 -j ACCEPT
 
 
 # Configure the general settings
+echo 1 > /proc/sys/net/ipv4/ip_forward
 # Uncomment the next line to enable packet forwarding for IPv4
 sed -i "s/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g" /etc/sysctl.conf
 sysctl -p
 
 #enable NAT
-sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+iptables -A FORWARD -i eth0 -o eth1 -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i eth1 -o eth0 -j ACCEPT
+# Forwarding port 80
+iptables -t nat -A PREROUTING -p tcp  -d 10.0.1.5/32 --dport 80 -j DNAT --to-destination 10.0.0.4:80
+iptables -t nat -A POSTROUTING  -d 10.0.0.4 -p tcp --dport 80 -j MASQUERADE
+# Forwarding port 443
+iptables -t nat -A PREROUTING -p tcp  -d 10.0.1.5/32 --dport 443 -j DNAT --to-destination 10.0.0.4:443
+iptables -t nat -A POSTROUTING  -d 10.0.0.4 -p tcp --dport 443 -j MASQUERADE
+# Forwarding port 5201
+iptables -t nat -A PREROUTING -p tcp  -d 10.0.1.5/32 --dport 5201 -j DNAT --to-destination 10.0.0.4:5201
+iptables -t nat -A POSTROUTING  -d 10.0.0.4 -p tcp --dport 5201 -j MASQUERADE
+# Forwarding port 5201 udp
+iptables -t nat -A PREROUTING -p udp  -d 10.0.1.5/32 --dport 5201 -j DNAT --to-destination 10.0.0.4:5201
+iptables -t nat -A POSTROUTING  -d 10.0.0.4 -p udp --dport 5201 -j MASQUERADE
+
 iptables-save > /etc/iptables_rules.save
 cat /etc/network/interfaces
-
-iptables -t nat -A PREROUTING -j DNAT -d 10.0.1.5 -p tcp --dport 80 --to 10.0.0.4
-iptables -t nat -A PREROUTING -j DNAT -d 10.0.1.5 -p tcp --dport 443 --to 10.0.0.4
-iptables -t nat -A PREROUTING -j DNAT -d 10.0.1.5 -p tcp --dport 5985 --to 10.0.0.4
-iptables -t nat -A PREROUTING -j DNAT -d 10.0.1.5 -p tcp --dport 5986 --to 10.0.0.4
-iptables -t nat -A PREROUTING -j DNAT -d 10.0.1.5 -p tcp --dport 5201 --to 10.0.0.4
-iptables -t nat -A PREROUTING -j DNAT -d 10.0.1.5 -p udp --dport 5201 --to 10.0.0.4
 
 
 exit 0 
