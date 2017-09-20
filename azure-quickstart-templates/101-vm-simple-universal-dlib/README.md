6a2d035a1b0f7fcaa5d617ab58375e0b6227f0e0
Frédéric Le Coquil <flecoqui@microsoft.com>
2017-09-20 19:59:30 +0200

    Add DLIB ARM template to create a VM with DLIB pre-requisites

---
+++ azure-quickstart-templates/101-vm-simple-universal-dlib/README.md
@@ -0,0 +1,118 @@
+# Deployment of a VM (Linux or Windows) installing DLIB pre-requisites (g++,python)
+
+<a href="https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2Fflecoqui%2Fazure%2Fmaster%2Fazure-quickstart-templates%2F101-vm-simple-universal%2Fazuredeploy.json" target="_blank">
+    <img src="http://azuredeploy.net/deploybutton.png"/>
+</a>
+<a href="http://armviz.io/#/?load=https%3A%2F%2Fraw.githubusercontent.com%2Fflecoqui%2Fazure%2Fmaster%2Fazure-quickstart-templates%2F101-vm-simple-universal%2Fazuredeploy.json" target="_blank">
+    <img src="http://armviz.io/visualizebutton.png"/>
+</a>
+
+
+This template allows you to deploy a simple VM running: </p>
+#### Debian: Apache, g++, cmake, Anaconda3, DLIB source code and samples (C++,Python),
+#### Ubuntu: Apache, g++, cmake, Anaconda3, DLIB source code and samples (C++,Python),
+#### Centos: Apache, g++, cmake, Anaconda3, DLIB source code and samples (C++,Python),
+#### Red Hat: Apache, g++, cmake, Anaconda3, DLIB source code and samples (C++,Python),
+#### Windows Server 2016: Apache, g++, cmake, Anaconda3, DLIB source code and samples (C++,Python),
+#### Nano Server 2016: Apache, g++, cmake, Anaconda3, DLIB source code and samples (C++,Python)
+This will deploy in the region associated with Resource Group and the VM Size is one of the parameter.</p>
+So far only the Debian and Ubuntu version have been tested.
+
+With Azure CLI you can deploy this VM with 2 command lines:
+
+
+![](https://raw.githubusercontent.com/flecoqui/azure/master/azure-quickstart-templates/101-vm-simple-universal/Docs/1-architecture.png)
+
+
+
+## CREATE RESOURCE GROUP:
+azure group create "ResourceGroupName" "DataCenterName"
+
+For instance:
+
+    azure group create iperfgrpeu2 eastus2
+
+## DEPLOY THE VM:
+azure group deployment create "ResourceGroupName" "DeploymentName"  -f azuredeploy.json -e azuredeploy.parameters.json
+
+For instance:
+
+    azure group deployment create iperfgrpeu2 depiperftest -f azuredeploy.json -e azuredeploy.parameters.json -vv
+
+Beyond login/password, the input parameters are :</p>
+configurationSize (Small: F2 and 128 GB data disk, Medium: F4 and 256 GB data disk, Large: F8 and 512 GB data disk, XLarge: F16 and 1024 GB data disk) : 
+
+    "configurationSize": {
+      "type": "string",
+      "defaultValue": "Small",
+      "allowedValues": [
+        "Small",
+        "Medium",
+        "Large",
+        "XLarge"
+      ],
+      "metadata": {
+        "description": "Configuration Size: VM Size + Disk Size"
+      }
+    }
+
+configurationOS (debian, ubuntu, centos, redhat, nano server 2016, windows server 2016): 
+
+    "configurationOS": {
+      "type": "string",
+      "defaultValue": "debian",
+      "allowedValues": [
+        "debian",
+        "ubuntu",
+        "centos",
+        "redhat",
+        "nanoserver2016",
+        "windowsserver2016"
+      ],
+      "metadata": {
+        "description": "The Operating System to be installed on the VM. Default value debian. Allowed values: debian,ubuntu,centos,redhat,nanoserver2016,windowsserver2016"
+      }
+    },
+
+
+
+## TEST THE VM:
+Once the VM has been deployed, you can open the Web page hosted on the VM.
+For instance for Linux VM:
+
+     http://vmubus001.eastus2.cloudapp.azure.com/index.php 
+
+for Windows VM:
+
+     http://vmnanos001.eastus2.cloudapp.azure.com/index.html 
+
+</p>
+You can also use the following bash files to: </p>
+#### buildDLIB.sh: to build DLIB library(C++),
+#### buildDLIBCPPSamples.sh: to build DLIB C++ samples (C++),
+#### buildDLIBPythonSamples.sh: to build DLIB Python samples (Python),
+#### runTests.sh: to run unit tests (C++,Python),
+
+</p>
+Finally, you can open a remote session with the VM.
+
+For instance for Linux VM:
+
+     ssh VMAdmin@vmubus001.eastus2.cloudapp.azure.com
+
+For Windows Server VM:
+
+     mstsc /admin /v:vmwins001.eastus2.cloudapp.azure.com
+
+For Nano Server VM:
+
+     Set-Item WSMan:\\localhost\\Client\\TrustedHosts vmnanos001.eastus2.cloudapp.azure.com </p>
+     Enter-PSSession -ComputerName vmnanos001.eastus2.cloudapp.azure.com </p>
+
+
+## DELETE THE RESOURCE GROUP:
+azure group delete "ResourceGroupName" "DataCenterName"
+
+For instance:
+
+    azure group delete iperfgrpeu2 eastus2
---
+++ azure-quickstart-templates/101-vm-simple-universal-dlib/install-software.sh
@@ -0,0 +1,318 @@
+#!/bin/bash
+# This bash file install apache
+# Parameter 1 hostname 
+azure_hostname=$1
+
+#############################################################################
+log()
+{
+	# If you want to enable this logging, uncomment the line below and specify your logging key 
+	#curl -X POST -H "content-type:text/plain" --data-binary "$(date) | ${HOSTNAME} | $1" https://logs-01.loggly.com/inputs/${LOGGING_KEY}/tag/redis-extension,${HOSTNAME}
+	echo "$1"
+}
+#############################################################################
+check_os() {
+    grep ubuntu /proc/version > /dev/null 2>&1
+    isubuntu=${?}
+    grep centos /proc/version > /dev/null 2>&1
+    iscentos=${?}
+    grep redhat /proc/version > /dev/null 2>&1
+    isredhat=${?}	
+	if [ -f /etc/debian_version ]; then
+    isdebian=0
+	else
+	isdebian=1	
+    fi
+
+	if [ $isubuntu -eq 0 ]; then
+		OS=Ubuntu
+		VER=$(lsb_release -a | grep Release: | sed  's/Release://'| sed -e 's/^[ \t]*//' | cut -d . -f 1)
+	elif [ $iscentos -eq 0 ]; then
+		OS=Centos
+		VER=$(cat /etc/centos-release)
+	elif [ $isredhat -eq 0 ]; then
+		OS=RedHat
+		VER=$(cat /etc/redhat-release)
+	elif [ $isdebian -eq 0 ];then
+		OS=Debian  # XXX or Ubuntu??
+		VER=$(cat /etc/debian_version)
+	else
+		OS=$(uname -s)
+		VER=$(uname -r)
+	fi
+	
+	ARCH=$(uname -m | sed 's/x86_//;s/i[3-6]86/32/')
+
+	log "OS=$OS version $VER Architecture $ARCH"
+}
+#############################################################################
+configure_apache(){
+# Apache installation 
+apt-get -y update
+apt-get -y install apache2
+if [ $isubuntu -eq 0 ]; then
+log "Install PHP 7.0"
+apt-get -y install php7.0-common libapache2-mod-php7.0 php7.0-cli
+else
+log "Install PHP 5"
+apt-get -y install php5-common libapache2-mod-php5 php5-cli
+fi
+apt-get -y install curl
+azure_localip=`ifconfig eth0 |  grep 'inet ' | awk '{print \$2}' | sed 's/addr://'`
+azure_publicip=`curl -s checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//'`
+log "Local IP address: $azure_localip"
+log "Public IP address: $azure_publicip"
+#
+# Start Apache server
+#
+apachectl restart
+
+directory=/var/www/html
+if [ ! -d $directory ]; then
+mkdir $directory
+fi
+
+cat <<EOF > $directory/index.php 
+<html>
+  <head>
+    <title>Sample "Hello from $azure_hostname" </title>
+  </head>
+  <body bgcolor=white>
+
+    <table border="0" cellpadding="10">
+      <tr>
+        <td>
+          <h1>Hello from $azure_hostname</h1>
+		  <p>OS $OS Version $VER Architecture $ARCH </p>
+		  <p>Local IP Address: </p>
+
+<?php
+echo "$azure_localip";
+
+?>
+
+<p>Public IP Address: </p>
+<?php
+echo "$azure_publicip";
+
+?>
+        </td>
+      </tr>
+    </table>
+
+    <p>This is the home page for the DLIB test on Azure VM</p>
+    <p>Launch the following commands: </p>
+    <p>     bash buildDLIB.sh: to build DLIB library </p> 
+    <p>     bash buildDLIBCPPSamples.sh: to build DLIB C++ samples </p> 
+    <p>     bash buildDLIBPythonSamples.sh: to build DLIB Python samples </p> 
+    <p>     bash runDLIBTests.sh: to run DLIB tests </p> 
+    <ul>
+      <li>To <a href="http://www.microsoft.com">Microsoft</a>
+      <li>To <a href="https://portal.azure.com">Azure</a>
+    </ul>
+  </body>
+</html>
+EOF
+
+
+echo "Configuring Web Site for Apache: $(date)"
+cat <<EOF > /etc/apache2/conf-available/html.conf 
+ServerName "$azure_hostname"
+<VirtualHost *:80>
+        ServerAdmin webmaster@localhost
+        ServerName "$azure_hostname"
+
+        DocumentRoot /var/www/html
+        <Directory />
+                Options FollowSymLinks
+                AllowOverride None
+        </Directory>
+
+       # Add CORS headers for HTML5 players
+        Header always set Access-Control-Allow-Headers "origin, range"
+        Header always set Access-Control-Allow-Methods "GET, HEAD, OPTIONS"
+        Header always set Access-Control-Allow-Origin "*"
+        Header always set Access-Control-Expose-Headers "Server,range"
+
+        # Possible values include: debug, info, notice, warn, error, crit,
+        # alert, emerg.
+        LogLevel warn
+        LogLevel warn
+        ErrorLog /var/log/apache2/azure-evaluation-error.log
+        CustomLog /var/log/apache2/azure-evaluation-access.log combined
+</VirtualHost>
+EOF
+
+}
+#############################################################################
+configure_apache_centos(){
+# Apache installation 
+yum clean all
+yum -y install httpd
+yum -y install php
+
+azure_localip=`ifconfig eth0 |  grep 'inet ' | awk '{print \$2}' | sed 's/addr://'`
+azure_publicip=`curl -s checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//'`
+log "Local IP address: $azure_localip"
+log "Public IP address: $azure_publicip"
+#
+# Start Apache server
+#
+systemctl start httpd
+systemctl enable httpd
+systemctl status httpd
+
+directory=/var/www/html
+if [ ! -d $directory ]; then
+mkdir $directory
+fi
+
+cat <<EOF > $directory/index.php 
+<html>
+  <head>
+    <title>Sample "Hello from $azure_hostname" </title>
+  </head>
+  <body bgcolor=white>
+
+    <table border="0" cellpadding="10">
+      <tr>
+        <td>
+          <h1>Hello from $azure_hostname</h1>
+		  <p>OS $OS Version $VER Architecture $ARCH </p>
+		  <p>Local IP Address: </p>
+
+<?php
+echo "$azure_localip";
+
+?>
+
+<p>Public IP Address: </p>
+<?php
+echo "$azure_publicip";
+
+?>
+        </td>
+      </tr>
+    </table>
+
+    <p>This is the home page for the DLIB test on Azure VM</p>
+    <p>Launch the following commands: </p>
+    <p>     bash buildDLIB.sh: to build DLIB library </p> 
+    <p>     bash buildDLIBCPPSamples.sh: to build DLIB C++ samples </p> 
+    <p>     bash buildDLIBPythonSamples.sh: to build DLIB Python samples </p> 
+    <p>     bash runDLIBTests.sh: to run DLIB tests </p> 
+    <ul>
+      <li>To <a href="http://www.microsoft.com">Microsoft</a>
+      <li>To <a href="https://portal.azure.com">Azure</a>
+    </ul>
+  </body>
+</html>
+EOF
+
+
+echo "Configuring Web Site for Apache: $(date)"
+cat <<EOF > /etc/httpd/conf.d/html.conf 
+ServerName "$azure_hostname"
+<VirtualHost *:80>
+        ServerAdmin webmaster@localhost
+        ServerName "$azure_hostname"
+
+        DocumentRoot /var/www/html
+        <Directory />
+                Options FollowSymLinks
+                AllowOverride None
+        </Directory>
+
+       # Add CORS headers for HTML5 players
+        Header always set Access-Control-Allow-Headers "origin, range"
+        Header always set Access-Control-Allow-Methods "GET, HEAD, OPTIONS"
+        Header always set Access-Control-Allow-Origin "*"
+        Header always set Access-Control-Expose-Headers "Server,range"
+
+        # Possible values include: debug, info, notice, warn, error, crit,
+        # alert, emerg.
+        LogLevel warn
+        LogLevel warn
+        ErrorLog /var/log/httpd/azure-evaluation-error.log
+        CustomLog /var/log/httpd/azure-evaluation-access.log combined
+</VirtualHost>
+EOF
+
+}
+#############################################################################
+configure_network(){
+# firewall configuration 
+iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+iptables -A INPUT -p tcp --dport 5201 -j ACCEPT
+iptables -A INPUT -p udp --dport 5201 -j ACCEPT
+}
+#############################################################################
+configure_network_centos(){
+# firewall configuration 
+iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+iptables -A INPUT -p tcp --dport 5201 -j ACCEPT
+iptables -A INPUT -p udp --dport 5201 -j ACCEPT
+
+service firewalld start
+firewall-cmd --permanent --add-port=5201/tcp
+firewall-cmd --permanent --add-port=5201/udp
+firewall-cmd --permanent --add-port=80/tcp
+firewall-cmd --permanent --add-port=443/tcp
+firewall-cmd --reload
+}
+#############################################################################
+start_apache(){
+apachectl restart
+}
+#############################################################################
+start_apache_centos(){
+systemctl restart httpd
+systemctl enable httpd
+systemctl status httpd
+}
+
+#############################################################################
+
+environ=`env`
+
+log "Environment before installation: $environ"
+
+log "Installation script start : $(date)"
+log "Apache Installation: $(date)"
+log "#####  azure_hostname: $azure_hostname"
+log "Installation script start : $(date)"
+check_os
+if [ $iscentos -ne 0 ] && [ $isredhat -ne 0 ] && [ $isubuntu -ne 0 ] && [ $isdebian -ne 0 ];
+then
+    log "unsupported operating system"
+    exit 1 
+else
+	if [ $iscentos -eq 0 ] ; then
+	    log "configure network centos"
+		configure_network_centos
+	    log "configure apache centos"
+		configure_apache_centos
+	elif [ $isredhat -eq 0 ] ; then
+	    log "configure network redhat"
+		configure_network_centos
+	    log "configure apache redhat"
+		configure_apache_centos
+	else
+	    log "configure network"
+		configure_network
+	    log "configure apache"
+		configure_apache
+	fi
+    log "start apache"
+      if [ $iscentos -eq 0 ] ; then
+	    start_apache_centos
+	  elif [ $isredhat -eq 0 ] ; then
+	    start_apache_centos
+      else
+	    start_apache
+	  fi
+fi
+exit 0 
+
